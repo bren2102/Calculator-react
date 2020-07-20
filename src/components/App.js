c58b51f021f2dd9e7bc44a81be5b657a20360d0b
bren2102 <brb.yg.21@gmail.com>
2020-07-19 20:49:55 -0500

    Implement the App component

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from './Button';
+import { ButtonPanel } from './ButtonPanel'; 
+
+class App extends React.Component {
+  render () {
+    return {}
+  }
+}
