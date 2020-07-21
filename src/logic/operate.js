29bcdf7ce258d57fffeaad29cd12afb271a90a51
bren2102 <brb.yg.21@gmail.com>
2020-07-21 11:16:10 -0500

    Implement Operate module

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,24 @@
 import Big from './big.mjs';
+
+const Operate = (numberOne, numberTwo, operation) => {
+  const nOne = Big(numberOne);
+  const nTwo = Big(numberTwo);
+  if (operation === '-') {
+    return (nOne - nTwo);
+  }
+  switch (operation) {
+    case '+':
+      return (nOne.plus(nTwo)).toString();
+    case '-':
+      return (nOne.minus(nTwo)).toString();
+    case 'x':
+      return (nOne.times(nTwo)).toString();
+    case '÷':
+      return (nOne.div(nTwo)).toString();
+    case '%':
+      return ((nOne.div(100)).times(nTwo)).toString();
+    default:
+      return 'Error';
+  }
+};
+export default Operate;
