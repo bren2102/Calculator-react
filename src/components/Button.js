7a9969c2d16ec60c524b1d166784a8e76774cd48
bren2102 <brb.yg.21@gmail.com>
2020-07-22 12:35:16 -0500

    Update Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = props => {
-  const { name, color, wide } = props;
+  const { name, color, wide, clickHandler } = props;
   const styling = {
     background: color,
     width: wide ? '50%' : '25%',
   };
   return (
-    <button type="button" className="button" style={styling}>
+    <button type="button" className="button" style={styling} onClick={()=>clickHandler(name)}>
       { name }
     </button>
   );
@@ -18,6 +18,7 @@ Button.propTypes = {
   name: PropTypes.string,
   color: PropTypes.string,
   wide: PropTypes.bool,
+  clickHandler: PropTypes.func.isRequired,
 };
 Button.defaultProps = {
   name: 'name',
