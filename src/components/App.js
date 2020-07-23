ea4369b2e7ca29ae686caca5b55bad1c7ed2bab7
Brenda Yucra <42762753+bren2102@users.noreply.github.com>
2020-07-23 17:10:11 -0500

    Revert "Fourth milestone"

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-/* eslint-disable */
-import Calculate from '../logic/calculate';
-/* eslint-enable */
+
 const App = () => (
   <div className="app">
     <Display />
